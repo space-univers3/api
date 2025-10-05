f9666d362c874243c8b6c7b9622c93810b5dbd21
Nguyen Thinh <40386174+kidp2h@users.noreply.github.com>
2025-10-05 10:29:11 +0700

    refactor: update format

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,81 +13,81 @@ import { Role } from "./roles";
 import { User, UserModule } from "./users";
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			load: configs,
-			isGlobal: true,
-			validationSchema,
-			envFilePath: ".env",
-			validationOptions: {
-				allowUnknown: true,
-				abortEarly: false,
-			},
-		}),
-		TypeOrmModule.forRootAsync({
-			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => {
-				const dbConfig = configService.get<DatabaseConfig>("database");
-				const appConfig = configService.get<AppConfig>("app");
-				console.log("Database URL:", dbConfig?.url);
-				console.log("Config: ", dbConfig);
-				return {
-					type: "postgres",
-					host: dbConfig?.host,
-					port: dbConfig?.port,
-					username: dbConfig?.username,
-					password: dbConfig?.password,
-					database: dbConfig?.database,
-					url: dbConfig?.url,
-					entities: [User, Role],
-					synchronize: appConfig?.env !== "production" && dbConfig?.synchronize,
-					logging: appConfig?.env !== "production" && dbConfig?.logging,
-					ssl: dbConfig?.ssl
-						? {
-								rejectUnauthorized: false,
-							}
-						: false,
-				};
-			},
-		}),
-		// GraphQL
-		GraphQLModule.forRootAsync<ApolloDriverConfig>({
-			driver: ApolloDriver,
-			inject: [ConfigService],
-			useFactory: (configService: ConfigService) => {
-				const appConfig = configService.get<AppConfig>("app");
-				const isDevelopment = appConfig?.env !== "production";
+  imports: [
+    ConfigModule.forRoot({
+      load: configs,
+      isGlobal: true,
+      validationSchema,
+      envFilePath: ".env.local",
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: false,
+      },
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig = configService.get<DatabaseConfig>("database");
+        const appConfig = configService.get<AppConfig>("app");
+        console.log("Database URL:", dbConfig?.url);
+        console.log("Config: ", dbConfig);
+        return {
+          type: "postgres",
+          host: dbConfig?.host,
+          port: dbConfig?.port,
+          username: dbConfig?.username,
+          password: dbConfig?.password,
+          database: dbConfig?.database,
+          url: dbConfig?.url,
+          entities: [User, Role],
+          synchronize: appConfig?.env !== "production" && dbConfig?.synchronize,
+          logging: appConfig?.env !== "production" && dbConfig?.logging,
+          ssl: dbConfig?.ssl
+            ? {
+                rejectUnauthorized: false,
+              }
+            : false,
+        };
+      },
+    }),
+    // GraphQL
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const appConfig = configService.get<AppConfig>("app");
+        const isDevelopment = appConfig?.env !== "production";
 
-				return {
-					autoSchemaFile: join(process.cwd(), "src/schema.graphql"),
-					sortSchema: true,
-					playground: true,
-					introspection: isDevelopment,
-					installSubscriptionHandlers: true,
-					schemas: true,
-					csrfPrevention: false,
+        return {
+          autoSchemaFile: join(process.cwd(), "src/schema.graphql"),
+          sortSchema: true,
+          playground: true,
+          introspection: isDevelopment,
+          installSubscriptionHandlers: true,
+          schemas: true,
+          csrfPrevention: false,
 
-					cache: "bounded",
-					path: "/graphql",
-					graphiql: true,
-					context: ({ req, res }) => ({ req, res }),
-					formatError: (formattedError) => {
-						console.error("GraphQL Error:", formattedError);
-						const originalError = formattedError.extensions
-							?.originalError as Record<string, unknown> | null;
-						if (originalError)
-							return {
-								message:
-									(originalError?.message as string) || "Internal server error",
-								statusCode: originalError?.statusCode || 500,
-							};
-						return formattedError;
-					},
-				};
-			},
-		}),
-		UserModule,
-	],
-	providers: [AppService],
+          cache: "bounded",
+          path: "/graphql",
+          graphiql: true,
+          context: ({ req, res }) => ({ req, res }),
+          formatError: (formattedError) => {
+            console.error("GraphQL Error:", formattedError);
+            const originalError = formattedError.extensions
+              ?.originalError as Record<string, unknown> | null;
+            if (originalError)
+              return {
+                message:
+                  (originalError?.message as string) || "Internal server error",
+                statusCode: originalError?.statusCode || 500,
+              };
+            return formattedError;
+          },
+        };
+      },
+    }),
+    UserModule,
+  ],
+  providers: [AppService],
 })
 export class AppModule {}
